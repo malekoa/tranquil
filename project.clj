e2317d8bee6fa31b09cd5f196ef634fb802f0c9e
Malek Azadegan <azadeganmalek@gmail.com>
2025-01-29 20:22:15 -0800

    update project.clj metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject tranquil "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A CLI tool that generates poems"
+  :url "https://github.com/malekoa/tranquil"
+  :license {:name "MIT"
+            :url "https://opensource.org/license/mit"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.0.206"]
                  [net.clojars.wkok/openai-clojure "0.22.0"]
